f911f6f2bbbd742fbe0f10cc6c8b6d8871b41dc7
RahulVPol <rahul.pol@cimpress.com>
2021-01-01 20:10:24 +0530

    Section-3-Commit-12 Added click handler and updated state

--- project/react-complete-guide/src/App.js
+++ project/react-complete-guide/src/App.js
@@ -4,15 +4,6 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
-  // Previously, we have seen props property, which allows you to pass data to component from parent component.
-  // Like props there is another special property of react which is state.State basically maintains internal
-  // state of the component.
-  // And changes in props  and/ or state  trigger React to re-render your components and potentially update the DOM in the browser
-
-  // state  simply is a property of the component class, you have to call it state  though - the name is not optional.
-  // You can then access it via this.state in your class JSX code
-  // Whenever state  changes (taught over the next lectures), the component will re-render and reflect the new state.
-  // The difference to props  is, that this happens within one and the same component - you don't receive new data (props ) from outside!
   state = {
     persons: [
       { name: "Rahul Vishnu Pol", age: 32 },
@@ -21,22 +12,33 @@ class App extends Component {
     ],
   };
 
+  switchNameHandler = () => {
+    // DON'T DO THIS: this.state.persons[1] = { name: "Pranali Dagadu Kadam", age: 31 },
+    this.setState({
+      persons: [
+        { name: "Rahul Vishnu Pol", age: 32 },
+        { name: "Pranali Dagadu Kadam", age: 31 },
+        { name: "Aadhya Rahul Pol", age: 1 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, this is react app</h1>
         <p>This is really working!!</p>
 
+        {/* In order to add click handler use onClick and remember not to use
+        parenthesis or the function will be executed automatically
+        Also, understand that changing state of app now updates props of Person component 
+        which results in updating the Person component as well
+         */}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+
         {this.state.persons.map((person, index) => {
           return <Person key={index} name={person.name} age={person.age} />;
         })}
-
-        {/* You can not use following syntax in jsx */}
-        {/* {
-          for(let i=0; i<this.state.persons.length; i++){
-            <Person key={index} name={person.name} age={person.age} />
-          }
-        } */}
       </div>
     );
   }
