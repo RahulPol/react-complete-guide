c06204a6524549f3f3ebd6316a8aa079e98c2bfa
RahulVPol <rahul.pol@cimpress.com>
2021-01-01 14:59:42 +0530

    Section-3-Commit-5 Understanding of jsx

--- project/react-complete-guide/src/App.js
+++ project/react-complete-guide/src/App.js
@@ -3,10 +3,20 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, this is react app</h1>
-      </div>
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, this is react app</h1>
+    //   </div>
+    // );
+
+    // the jsx written above gets converted into javascript code as shown below
+    // but as you can see it is very difficult to write code like this, thus we use
+    // jsx syntax which is easy to understand and comes naturally as we have worked on html
+    // In order to convert the jsx to js we use babel tool
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "Hi, this is react app")
     );
   }
 }
