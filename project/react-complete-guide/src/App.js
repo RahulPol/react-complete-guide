946bb866c9ef794e820929706d4ed3d5d5d83acf
RahulVPol <rahul.pol@cimpress.com>
2021-01-01 21:17:18 +0530

    Section-3-Commit-18 Implemented two way binding

--- project/react-complete-guide/src/App.js
+++ project/react-complete-guide/src/App.js
@@ -23,6 +23,16 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Rahul Vishnu Pol", age: 32 },
+        { name: event.target.value, age: 31 },
+        { name: "Aadhya Rahul Pol", age: 1 },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -31,18 +41,11 @@ class App extends Component {
         <button onClick={this.switchNameHandler.bind(this, "Rahul V. Pol")}>
           Switch Name
         </button>
-        {/* {this.state.persons.map((person, index) => {
-          return <Person key={index} name={person.name} age={person.age} />;
-        })} */}
-        {/* It is possible to not only pass data but also methods to your child components
-        These methods can then update the state in parent component. This is very important
-        pattern which allows your child component to update parent components state */}
-        {/* Ok! we can pass method reference to child components but how do we pass arguments to that 
-        method reference so if I have a method that updates parent component's state and I want to 
-        pass argument to it, how would I do it?
 
-        for this purpose you can use bind method(recommended) or arrow function(inefficient in performance)
-        */}
+        {/* Ok! I have passed the method reference that will update the state of
+        parent component but the method reference takes static name what if I
+        want to update dynamically from my child component */}
+
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -50,9 +53,8 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          // passing switchNameHandler to child component which upon click of child component will update
-          // parent component's state
           click={() => this.switchNameHandler("Rahul Pol")}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[2].name}
